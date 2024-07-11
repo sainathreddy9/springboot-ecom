f1c5f786263ef3b2c0203ea1675c0649b34189cf
pruthvisainathreddyjalla <sainathreddy98@gmail.com>
2024-07-11 13:04:53 -0500

    Ecom first commit

---
+++ src/main/java/com/saiprojects/ecomproj/newTest.java
@@ -0,0 +1,11 @@
+package com.saiprojects.ecomproj;
+
+public class newTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 0;
+        for (int i = 0; i <=5;i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/test/java/com/saiprojects/ecomproj/EcomProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiprojects.ecomproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
